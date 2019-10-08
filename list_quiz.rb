89d8d38eb667dc57783c051aeb594940d08957c8
Percy Nizamov <MysticGummiBear@gmail.com>
2019-10-08 11:27:04 -0600

    finished all three

--- list_quiz.rb
+++ list_quiz.rb
@@ -49,7 +49,56 @@ def three_even?(list)
 
 end
 
-p three_even?([5,1,2,2,4,5]) #true
-p three_even?([])  #false
-p three_even?([2,4,12,5]) #true
-p three_even?([2,1,4,6]) #false
+# p three_even?([5,1,2,2,4,5]) #true
+# p three_even?([])  #false
+# p three_even?([2,4,12,5]) #true
+# p three_even?([2,1,4,6]) #false
+
+
+# def series_up(list)
+#     if n[0] != n[1] # for spots one and bewlow
+#         return false
+#     end
+
+#     list.size.times do |n|
+#       num = n+1
+#       list.size.times do |m|
+#         slice = list[m+n]
+#         print slice 
+#       end 
+#     end
+
+# end
+
+# print series_up([1,1,1,2,1,2,3])
+
+
+def series_up(list)
+    difference = 0
+    list_sum = 0
+    firsr_term = 0
+    list_size = 0
+    list.each do |f|
+        list_size += 1
+    end
+
+    if n[0] != n[1] # for spots one and bewlow
+        return false
+    end
+
+    list.each do |n|
+      list_sum += n
+    end
+
+    difference = list[2]- list[1]
+    firsr_term = list[0]
+
+    calculated_sum = 1/2((2*firsr_term)+((list_size)-1)difference)
+
+    if calculated_sum == list_sum
+        return true
+    end
+
+
+        
+end
