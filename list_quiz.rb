feb89c03c5ca85cce25776b88bf902fe5d98984f
Percy Nizamov <MysticGummiBear@gmail.com>
2019-10-11 06:21:20 -0600

    Bug Testing

--- list_quiz.rb
+++ list_quiz.rb
@@ -27,6 +27,9 @@ def three_even?(list)
     count = 0
     index = 0 
     num = 0
+    tester_list = [] 
+    counter = 0    
+    
     if list.length < 3
         return false
     end
@@ -34,42 +37,56 @@ def three_even?(list)
     list.each do |j|
         index +=1
     end
+    
 
-    while count < list.length
-        list_addition = 0
-        tester_list = 0
-        counter = 0
-        list.size.times do |n|
-            slice = list[count..(count+2)] # instance aa224
-            slice.size.times do |n| #i instead tried to check for three seperate values instead ; I have no clue whether this works
-            tester = list[n] % 2  #cause I really need to finish series up - legitimatly 
+    list_addition = 0
+    list.size.times do |m|
+        slice = list[count..(count+2)]                     
+        (slice.size-2).times do |n| #removoved the while loop
+            tester = list[n] % 2  
+            
             tester_list.push(tester)
-                if tester_list.length == 3
-                    tester_list.size.times do |n|
-                    counter += n 
-                end 
-                if counter == 0 
-                    return true
-                end 
-            end 
+               
+            if tester_list.length == 3 #flubbed logic ; have to finish this
+                tester_list.size.times do |n|
+                    if tester_list[0] == 0 && tester_list[1] == 0 && tester_list[2] == 0 
+                        counter +=1
+                        puts counter
+                    end
+                    if counter == 0 
+                        return true
+                    end 
+    
+                     
+                end  
+                tester_list = []
+                #figure out why it's returning numberical values, most not likely fitting ant conditions. 
+                
+            end
 
+            counter = 0
+        end 
+                            #   if num == 3 #it's never going to reach that condition this rendering it always false
+                            #     return true
+                            #   end
 
-            #   if num == 3 #it's never going to reach that condition this rendering it always false
-            #     return true
-            #   end
-        end
+    
+
+                        # if counter > 1
+                        #     return false
+                     # end
         
-        
-        # if list_addition % 2 == 0  && count < index-2 #I shouldn've done a modulus for there are too many exeptions
-        #     return true
-        # end
-    end
-
+                    # if list_addition % 2 == 0  && count < index-2 #I shouldn've done a modulus for there are too many exeptions
+                #     return true
+                    # end
+    end 
+    return false
+    
 end
 
 p three_even?([5,1,2,2,4,5]) #true
 p three_even?([])  #false
-p three_even?([2,4,12,5]) #true
+p three_even?([2,4,12,5]) #true # not registering taht tthere are 3 variables
 p three_even?([2,1,4,6]) #false
 
 
@@ -159,12 +176,30 @@ def series_up_inverse(num)
     final_list = []
     a = 1
     preface_count  = 0 
-    count  = 0 
+    d= 1
     index = 0
-    while index < count 
+    c= 0 
+    list_num  = []
+    calculated_sum = 0
+    
+    first_part = (2.0*a)
+    second_part_first = (num-1)-1 
+    second_part_final = second_part_first*d
+    coefficient = (num-1)/2.0
+    added = coefficient*(first_part+second_part_final) #I mistyped the equation, it should be n over two I forgot to find the number of terms in the sequence,
+    
+    calculated_sum = num*num - added
+
         
+    
+    while index < calculated_sum
+        final_list.push(a+c*d) #I need to fix this so this loops, starting from the coeffiecient of one anr repeating
+        c += 1
+        index += 1
     end
+    
+    return final_list
 
 end
 
-puts series_up_inverse(3)
+print series_up_inverse(4)
